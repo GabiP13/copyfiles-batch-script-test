66abc105b59dd48268c177ff258a84831874f75b
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-05-27 11:57:28 -0700

    all applicable xcopy flags for my situation

--- copyfiles.bat
+++ copyfiles.bat
@@ -2,3 +2,6 @@ echo word
 mkdir .\"testfolder"
 xcopy /s/e /y /v /h /k "c:\Users\user\Downloads\testfolder" "c:\Users\user\Downloads\testfolder"
 REM need to test ^
+REM /v verifies the copies file are the same; /f shows file names while copying; /i creates new directory;
+REM /e (used with /s and /t) copies all subdirectories; /k retains read-only; /h copies hidden files; /o and /x copy control list;
+REM /y automatically overwrites dest files; /z ?; /j ?; /compress ?; /noclone ?
