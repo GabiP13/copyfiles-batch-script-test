6748f5f3ca7be86ad5de5ff60271880281b1199c
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-08-08 00:40:33 -0700

    added variables to create multiple new files

--- safe testing folder/populate_files.bat
+++ safe testing folder/populate_files.bat
@@ -1,1 +1,12 @@
+@echo off
 REM goal: create files in the two subfolders for testing purposes
+
+for /F "delims=" %%e in ('dir /ad /b') do (
+    set fn=%%e.txt
+    @echo on
+    echo ---%%e
+    echo testing area
+    echo %fn%---
+    @echo off
+    copy /y nul %fn% REM <-- REMOVE SPACE FROM %FN%
+)
