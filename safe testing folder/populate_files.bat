b05617986e6f183e9166e4c9f4925038dfe12f46
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-08-10 01:14:35 -0700

    fixed loop except for added := at end of string

--- safe testing folder/populate_files.bat
+++ safe testing folder/populate_files.bat
@@ -1,12 +1,19 @@
 @echo off
 REM goal: create files in the two subfolders for testing purposes
 
+SETLOCAL ENABLEDELAYEDEXPANSION
 for /F "delims=" %%e in ('dir /ad /b') do (
-    set fn=%%e.txt
     @echo on
-    echo ---%%e
-    echo testing area
-    echo %fn%---
+    echo ---%%e-
+    @REM echo E %%e
+    set fn=%%e.txt
+    echo FN !fn!
+    set myvar=%fn:fol=ooo%
+    echo MYVAR !myvar!??
+    @REM echo ^ before n AFTER:
+    @REM set fn=%fn:fol=ooo%
+    @REM echo FN %fn%
+    echo -!fn!---
     @echo off
     copy /y nul %fn% REM <-- REMOVE SPACE FROM %FN%
 )
